c6020a062de49804ac408ae6c6d658e01d922159
armon <armon.hu@outlook.com>
2021-11-25 17:59:13 +0800

    1. proxy

---
+++ csharp/Proxy/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Proxy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var subject = new ProxySubject(new RealSubject());
+            subject.Request();
+            
+        }
+    }
+}
---
+++ csharp/Proxy/ProxySubject.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proxy
+{
+    class ProxySubject: ISubject
+    {
+        private ISubject subject;
+        public ProxySubject(ISubject subject)
+        {
+            this.subject = subject;
+        }
+
+        public void Request()
+        {
+            Console.WriteLine("proxy subject");
+            this.subject.Request();
+        }
+    }
+}
